525bb076a953082f4788f1d3ffaadbaecd76e3ab
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-26 15:57:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
     </div>
   );
 }
