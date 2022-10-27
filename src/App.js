a9d2c4f185bb15b90175c5091a3ec5226d117782
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-27 08:03:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import logo from "./logo.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
